1338314de815e08538bf70fe178c6b82e09c165f
007Gergo <49782625+007Gergo@users.noreply.github.com>
2019-04-19 19:55:05 +0200

    ini reader class sceleton

--- iniReader/iniFileReader/iniFileReader.cpp
+++ iniReader/iniFileReader/iniFileReader.cpp
@@ -1,1 +1,28 @@
+#include <stdio.h>
 #include "iniFileReader.h"
+
+namespace INI
+{	
+iniFileReader::iniFileReader(const char * fileName) : iIniFileReader()
+{
+}
+
+iniFileReader::~iniFileReader()
+{
+}
+
+bool iniFileReader::load()
+{ 
+	return true;
+}
+
+bool iniFileReader::changed()
+{
+	return true;
+}
+
+void iniFileReader::print()
+{ 
+	printf("Print");
+}
+}
--- iniReader/iniFileReader/iniFileReader.h
+++ iniReader/iniFileReader/iniFileReader.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "..\iIniFileReader\iIniFileReader.h"
+
+namespace INI
+{
+class iniFileReader :
+	public iIniFileReader
+{
+public:
+	iniFileReader(const char * fileName);
+	virtual ~iniFileReader();
+	virtual bool load();
+	virtual bool changed();
+	virtual void print();
+};
+}
+
