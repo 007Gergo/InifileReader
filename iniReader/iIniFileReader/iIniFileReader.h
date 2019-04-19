138b828db2d314e7dce630c7e027f4c054343554
007Gergo <49782625+007Gergo@users.noreply.github.com>
2019-04-19 19:09:20 +0200

    first interface sceleton

--- iniReader/iIniFileReader/iIniFileReader.h
+++ iniReader/iIniFileReader/iIniFileReader.h
@@ -1,3 +1,14 @@
 #pragma once
-
+namespace INI
+{
+class iIniFileReader
+{
+public:
+	iIniFileReader() {};
+	virtual bool load() = 0;
+	virtual bool changed() = 0;
+	virtual void print() = 0;
+	virtual ~iIniFileReader() {};
+};
+}
 
